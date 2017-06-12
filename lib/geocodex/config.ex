10d33de1e38097c8ee2efebef41e0439cf9e68fc
David Santos Merino <dsantosmerino@gmail.com>
2017-06-12 21:38:58 +0200

    Refactor Config module

--- lib/geocodex/config.ex
+++ lib/geocodex/config.ex
@@ -1,15 +1,44 @@
 defmodule Geocodex.Config do
-  @moduledoc false
+  @moduledoc """
+  Allows to modify and retrieve your Geocodex configuration
+  """
 
-  def geocode_api_url do
-    Application.fetch_env! :geocodex, :geocode_api_url
-  end
+  @doc """
+  Modifies your API Key value
 
-  def api_key do
-    Application.fetch_env! :geocodex, :api_key
-  end
-
+      iex> Geocoder.Config.api_key("YOUR API KEY")
+      :ok
+  """
   def api_key(api_key) do
-    Application.put_env :geocodex, :api_key, api_key
+    Application.put_env(:geocodex, :api_key, api_key)
+  end
+
+  @doc """
+  Returns your API Key configuration value
+
+      iex> Geocoder.Config.api_key("YOUR API KEY")
+      :ok
+      iex> Geocoder.Config.api_key
+      "YOUR API KEY"
+  """
+  def api_key do
+    get_env(:api_key)
+  end
+
+  @doc """
+  Returns the API Url configuration value
+
+      iex> Geocoder.Config.geocode_api_url
+      "https://maps.googleapis.com/maps/api/geocode"
+  """
+  def geocode_api_url do
+    get_env(:geocode_api_url)
+  end
+
+  defp get_env(key) do
+    case Application.fetch_env(:geocodex, key) do
+      {:ok, value} -> value
+      _ -> nil
+    end
   end
 end
